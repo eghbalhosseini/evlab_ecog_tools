f1a262725d1faffb840776a5fbb31e3e7493bbad
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-06-18 10:22:04 -0400

    added AMC082 op info

--- create_sub_operation_info_ALBANY.m
+++ create_sub_operation_info_ALBANY.m
@@ -88,14 +88,45 @@ eval(strcat(op_info.subject_name,'_op','.op_info=op_info'));
 save(strcat(save_path,op_info.subject_name,'_operation_info.mat'),strcat(op_info.subject_name,'_op'),'-v7.3');
 
 %%
+%% AMC 82
 op_info=struct;
 op_info.subject_name='AMC082';
 
-op_info.GND=[];
-op_info.Ref=[];
+op_info.GND=[127];
+op_info.Ref=[128];
 op_info.num_channels=128;
-op_info.bad_channels = []; 
+op_info.ecog_channels=1:126;
+op_info.skull_eeg_channels=127:128;
+op_info.microphone_channels=[192,129];
+op_info.EMG_channels=[129:134];
+op_info.visual_trigger='DigIO_1';
+op_info.button_trigger='DigIO_2';
+op_info.buzzer_trigger='DigIO_3';
+op_info.audio_trigger='DigIO_4';
+op_info.bad_channels = [];
+op_info.task_file_location='/DAY6/MITNaturalisticStoriesTask/ECOG001/';
+labels= [arrayfun(@(x) sprintf('LG_%d',x),[1:64]','uniformoutput',false);...
+        arrayfun(@(x) sprintf('LFM_%d',x),[1:16]','uniformoutput',false);...
+        arrayfun(@(x) sprintf('LFS_%d',x),[1:16]','uniformoutput',false);...
+        arrayfun(@(x) sprintf('LFA_%d',x),[1:4]','uniformoutput',false);...
+        arrayfun(@(x) sprintf('LIP_%d',x),[1:12]','uniformoutput',false);...
+        arrayfun(@(x) sprintf('LIA_%d',x),[1:10]','uniformoutput',false);...
+        arrayfun(@(x) sprintf('RIH_%d',x),[1:4]','uniformoutput',false);...
+        'GND';'GND'];
 
+op_info.channel_labels=labels;
+op_info.seizure_channels=[121,122,105,106,87,95,96];
+op_info.exp_name='';
 eval(strcat(op_info.subject_name,'_op','.op_info=op_info'));
 save(strcat(save_path,op_info.subject_name,'_operation_info.mat'),strcat(op_info.subject_name,'_op'),'-v7.3');
+% op_info=struct;
+% op_info.subject_name='AMC082';
+% 
+% op_info.GND=[];
+% op_info.Ref=[];
+% op_info.num_channels=128;
+% op_info.bad_channels = []; 
+% 
+% eval(strcat(op_info.subject_name,'_op','.op_info=op_info'));
+% save(strcat(save_path,op_info.subject_name,'_operation_info.mat'),strcat(op_info.subject_name,'_op'),'-v7.3');
 end
